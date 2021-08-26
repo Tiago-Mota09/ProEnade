f1fbf933b61fd40c7896246b8427c64e3496a983
Tiago Mota <71279207+Tiago-Mota09@users.noreply.github.com>
2021-08-26 18:52:30 -0300

    feat: Criação de Classes das Camadas Profile e Filters

---
+++ ProEnade.Api/Profiles/ProfessorQuestoesProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ProEnade.API.Data.Entities;
+using ProEnade.API.Domain.Models.Request;
+using ProEnade.API.Domain.Models.Response;
+
+namespace ProEnade.API.Profiles
+{
+    public class ProfessorQuestoesProfile : Profile
+    {
+        public ProfessorQuestoesProfile()
+        {
+            CreateMap<ProfessorQuestoesRequest, ProfessorQuestoesEntity>().ReverseMap();
+            CreateMap<ProfessorQuestoesEntity, ProfessorQuestoesResponse>().ReverseMap();
+            CreateMap<ProfessorQuestoesUpdateRequest, ProfessorQuestoesEntity>().ReverseMap();
+        }
+    }
+}
+
+
+
+
+
