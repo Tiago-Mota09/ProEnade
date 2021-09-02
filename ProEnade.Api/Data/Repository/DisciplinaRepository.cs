c93a6c8201f2144bfb42f6d27cfa501af066b402
Tiago Mota <71279207+Tiago-Mota09@users.noreply.github.com>
2021-09-02 15:34:19 -0300

    Fix: Atualização

--- ProEnade.Api/Data/Repository/DisciplinaRepository.cs
+++ ProEnade.Api/Data/Repository/DisciplinaRepository.cs
@@ -1,10 +1,7 @@
 ﻿using Dapper;
 using ProEnade.API.Data.Entities;
 using Microsoft.Extensions.Configuration;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProEnade.API.Data.Repositories
 {
